6d97072976f79d852888325727317d0d5822b5d9
terencecistudent <terencelogue100@hotmail.co.uk>
2019-12-18 15:48:33 +0000

    Hellow Flask Unit

---
+++ run.py
@@ -0,0 +1,19 @@
+import os
+
+# Importing flask class
+from flask import Flask
+
+# Creating instance of this and storing in a var called app
+# First argument of the Flask class is the name of the applications module
+app = Flask(__name__)
+
+# A decorator starts with the @ sign, which is also called pie notation. 
+@app.route("/")
+def index():
+    return "Hello World"
+
+# Name of the default module in Python.
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', 
+            port=5000, 
+            debug=True)
