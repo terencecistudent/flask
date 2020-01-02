fd1227148c1e5674b6516085b08890ab43f0d4e0
terencecistudent <terencelogue100@hotmail.co.uk>
2020-01-02 15:47:42 +0000

    Routing and Templates

--- run.py
+++ run.py
@@ -2,11 +2,12 @@ import os
 import json
 
 # Importing flask class
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 # Creating instance of this and storing in a var called app
 # First argument of the Flask class is the name of the applications module
 app = Flask(__name__)
+app.secret_key = "some_secret"
 
 # A decorator starts with the @ sign, which is also called pie notation. 
 @app.route("/")
@@ -23,8 +24,26 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            # if url in that particular element of array is equal to member_name, then member obj should equal our obj
+            if obj["url"] == member_name:
+                member = obj
+
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():    
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+              request.form["name"]))
+
     return render_template("contact.html", page_title="Contact")
 
 
